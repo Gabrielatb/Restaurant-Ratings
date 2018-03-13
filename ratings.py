da16176464b87ddd862d58425999dc13640f573a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-12 17:38:16 -0700

    User answer added

--- ratings.py
+++ ratings.py
@@ -4,11 +4,14 @@
 def get_restaurant_scores(filename):
     data = open(filename)
     restaurant_scores = {}
+    user_restaurant = raw_input("What is your favorite restaurant? ")
+    user_rating = raw_input("What is your rating for this restaurant? ")
     for line in data:
         words = line.strip()
         name, rating = words.split(":")
         #sorted_restaurant_name_rating = sorted(restaurant_name_rating)
         restaurant_scores[name] = rating
+        restaurant_scores[user_restaurant] = user_rating
     #print restaurant_scores
     for name, rating in sorted(restaurant_scores.items()):
         print "{} is rated at {}".format(name, rating)
