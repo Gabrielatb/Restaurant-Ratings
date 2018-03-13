21da38b06e7a3c0a09773bc5d53d650f73192a2d
Hackbright Student <no-reply@hackbrightacademy.com>
2018-03-12 18:01:26 -0700

    User Exception

--- ratings.py
+++ ratings.py
@@ -6,12 +6,18 @@ def get_restaurant_scores(filename):
     restaurant_scores = {}
     user_restaurant = raw_input("What is your favorite restaurant? ")
     user_rating = raw_input("What is your rating for this restaurant? ")
+    try:
+        user_rating = int(user_rating) 
+    except:
+        #user_rating > 0 and user_rating <= 5:
+        #restaurant_scores[user_restaurant] = user_rating
+        user_rating = raw_input("Please type an integer between 1 and 5.")
     for line in data:
         words = line.strip()
         name, rating = words.split(":")
         #sorted_restaurant_name_rating = sorted(restaurant_name_rating)
         restaurant_scores[name] = rating
-        restaurant_scores[user_restaurant] = user_rating
+
     #print restaurant_scores
     for name, rating in sorted(restaurant_scores.items()):
         print "{} is rated at {}".format(name, rating)
